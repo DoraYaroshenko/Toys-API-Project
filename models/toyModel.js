bdbd651a9b518e90c353052e4b23295c1a5bec8b
DoraYaroshenko <124884535+DoraYaroshenko@users.noreply.github.com>
2023-03-14 23:50:06 +0200

    Add files via upload

--- models/toyModel.js
+++ models/toyModel.js
@@ -22,7 +22,7 @@ exports.validateJoi = (_reqBody) => {
         name:Joi.string().min(2).max(99).required(),
         info:Joi.string().min(2).max(999).required(),
         category:Joi.string().min(2).max(20).required(),
-        img_url:Joi.string().min(2).max(99).allow("", null),
+        img_url:Joi.string().min(2).max(999).allow("", null),
         price:Joi.number().min(1).max(2000).required(),
     })
     return joiSchema.validate(_reqBody);
